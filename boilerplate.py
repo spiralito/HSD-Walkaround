5935339a8b50a0f0649ee8777abd5d239f86cccf
virtuNat <janlothan@gmail.com>
2019-03-22 14:26:15 +0800

    Actual Sprite Test
    - Added Sprites and Tilemaps

--- boilerplate.py
+++ boilerplate.py
@@ -1,4 +1,7 @@
 import os
+import struct
+import itertools
+import numpy as np
 import pygame as pg
 import asyncio as aio
 
@@ -15,6 +18,41 @@ def load_image(name, *, alpha=None, colorkey=None):
             image.set_colorkey(colorkey)
         return image
     return image.convert_alpha()
+
+
+class TileMapSprite(pg.sprite.Sprite):
+    """The tiled background that all the other sprites walk on."""
+
+    tiles = None
+    def __init__(self, mapname):
+        if self.__class__.tiles is None:
+            self.__class__.tiles = load_image('tiles.png', colorkey=0xFF00FF)
+        with open(os.path.join('assets', 'mapdata', mapname), 'rb') as mapfile:
+            if mapfile.read(4) != b'TLMP':
+                raise IOError('Mapfile of incorrect format')
+            self.size = struct.unpack('>II', mapfile.read(8))
+            self.collmap = np.array([
+                [byte for byte in mapfile.read(self.size[1])]
+                for _ in range(self.size[0])
+                ]
+                , dtype=bool)
+            self.tilemap = pg.Surface((self.size[0] * 32, self.size[1] * 32))
+            for row, col in itertools.product(*map(range, self.size)):
+                tileid = struct.unpack('>I', mapfile.read(4))[0]
+                self.tilemap.blit(
+                    self.tiles,
+                    (col * 32, row * 32, 32, 32),
+                    (tileid % 8 * 32, tileid // 8 * 32, 32, 32)
+                    )
+        self.tilerect = self.tilemap.get_rect()
+        self.rect = self.tilemap.get_rect()
+        self.image = self.tilemap.copy()
+
+    def reset(self):
+        self.image.blit(self.tilemap, (0, 0))
+
+    def draw(self, window):
+        window.blit(self.image, self.rect)
 
 
 class ClipDrawSprite(pg.sprite.Sprite):
--- game.py
+++ game.py
@@ -5,22 +5,88 @@ import pygame as pg
 import asyncio as aio
 from boilerplate import (
     load_image, ClipDrawSprite, ClipDrawGroup,
-    GameState,
+    TileMapSprite, GameState,
     )
+
+key_config = {
+    'U': pg.K_UP,
+    'D': pg.K_DOWN,
+    'L': pg.K_LEFT,
+    'R': pg.K_RIGHT,
+    }
+
+class PlayerSprite(ClipDrawSprite):
+    """The player character."""
+
+    instance = None
+    def __new__(cls, *args):
+        if cls.instance is None:
+            cls.instance = object.__new__(cls)
+        return cls.instance
+
+    def __init__(self, pos):
+        super().__init__(
+            load_image('sprites.png', colorkey=0xFF00FF),
+            pg.Rect(0, 0, 32, 32),
+            pg.Rect(0, 0, 32, 32),
+            )
+        self.rect.midbottom = pos
+        self.vel = [0, 0]
+
+    def set_motion(self, axis, mdir, press):
+        if axis == 0:
+            if self.vel[1] * mdir > 0:
+                self.vel[1] = 0
+            elif press:
+                self.vel[1] = 3 * mdir
+        else:
+            if self.vel[0] * mdir > 0:
+                self.vel[0] = 0
+            elif press:
+                self.vel[0] = 3 * mdir
+
+    def update(self):
+        self.rect.centerx += self.vel[0]
+        self.rect.bottom += self.vel[1]
 
 
 class Walkaround(GameState):
     def __init__(self, handler):
         super().__init__(handler)
-        self.sprite = load_image('sprites.png', colorkey=0xFF00FF)
+        self.tilemap = TileMapSprite('testmap.map')
+        self.tilemap.rect.center = self.handler.rect.center
+        self.player = PlayerSprite(self.tilemap.tilerect.center)
 
     async def eval_logic(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.handler.running = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == key_config['U']:
+                    self.player.set_motion(0, -1, True)
+                elif event.key == key_config['D']:
+                    self.player.set_motion(0, +1, True)
+                elif event.key == key_config['L']:
+                    self.player.set_motion(1, -1, True)
+                elif event.key == key_config['R']:
+                    self.player.set_motion(1, +1, True)
+            elif event.type == pg.KEYUP:
+                if event.key == key_config['U']:
+                    self.player.set_motion(0, -1, False)
+                elif event.key == key_config['D']:
+                    self.player.set_motion(0, +1, False)
+                elif event.key == key_config['L']:
+                    self.player.set_motion(1, -1, False)
+                elif event.key == key_config['R']:
+                    self.player.set_motion(1, +1, False)
+        self.player.update()
 
     async def draw_frame(self):
-        self.handler.window.blit(self.sprite, (0, 0, 32, 32))
+        window = self.handler.window
+        window.fill(0x000000)
+        self.tilemap.reset()
+        self.player.draw(self.tilemap.image)
+        self.tilemap.draw(window)
         pg.display.flip()
 
 
@@ -35,6 +101,7 @@ class GameApplication(object):
 
     def __init__(self):
         self.window = pg.display.get_surface()
+        self.rect = self.window.get_rect()
         self.state = Walkaround(self)
         self.running = True
 
